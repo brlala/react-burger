5c14ee2c3581b0addb1f64b5220d917528e85f9e
liheng <liheng@pand.ai>
2020-12-19 12:41:21 +0800

    burger ingredient

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,7 +1,9 @@
+import classes from './Layout.module.css';
+
 const layout = (props) => (
   <>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>{props.children}</main>
+    <main className={classes.Content}>{props.children}</main>
   </>
 );
 
