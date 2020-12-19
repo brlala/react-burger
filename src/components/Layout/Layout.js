ec2bbe24ddff2a5e79f46a15bfd704f3a8843fca
liheng <liheng@pand.ai>
2020-12-19 16:21:00 +0800

    added logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,7 +1,9 @@
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 import classes from './Layout.module.css';
 
 const layout = (props) => (
   <>
+    <Toolbar />
     <div>Toolbar, SideDrawer, Backdrop</div>
     <main className={classes.Content}>{props.children}</main>
   </>
