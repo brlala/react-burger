edd58ee821fb003268c2c03ef068d4f2ce775499
liheng <liheng@pand.ai>
2020-12-22 19:01:08 +0800

    adding firebase rules

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -62,11 +62,11 @@ export const fetchOrdersStart = (error) => {
   };
 };
 
-export const fetchOrders = (token) => {
+export const fetchOrders = (token, userId) => {
   return (dispatch) => {
     dispatch(fetchOrdersStart());
     axios
-      .get(`orders.json?auth=${token}`)
+      .get(`orders.json?auth=${token}&orderBy="userId"&equalTo="${userId}"`)
       .then((res) => {
         const fetchedOrders = [];
         for (const key in res.data) {
